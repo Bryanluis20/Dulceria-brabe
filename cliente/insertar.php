45c64b66cebd7abd14f2616845898a0f78aee641
EstherAtiny <98972201+EstherAtiny@users.noreply.github.com>
2022-12-18 21:46:16 -0600

    Add files via upload

---
+++ cliente/insertar.php
@@ -0,0 +1,21 @@
+<?php
+include("../conexion.php");
+$con=conectar();
+
+$Id_Cliente=$_POST['Id_Cliente'];
+$nombres=$_POST['nombres'];
+$apellidos=$_POST['apellidos'];
+$telefono=$_POST['telefono'];
+$direcion=$_POST['direcion'];
+$municipio=$_POST['Municipio'];
+
+
+$sql="INSERT INTO cliente VALUES('$Id_Cliente','$nombres','$apellidos','$telefono','$direcion','$municipio')";
+$query= mysqli_query($con,$sql);
+
+if($query){
+    Header("Location: cliente.php");
+    
+}else {
+}
+?>
